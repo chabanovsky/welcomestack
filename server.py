4b6d618e75619bd60d25609969f8f352253fa776
Nicolas Chabanovsky <nicolas@chabanovsky.com>
2016-09-01 18:27:27 +0300

    Initial commit of the first version

---
+++ server.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*- 
+
+import SimpleHTTPServer
+import SocketServer
+import json, requests
+import os
+import sys
+import memcache
+from wsgiref.simple_server import make_server
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+se_api_url = "api.stackexchange.com/2.2/";
+request_protocol = "http://"
+api_key = ""
+target_site = "ru.stackoverflow"
+
+PORT = 8000
+PAGE_SIZE = 10
+DOCUMENT_ROOT = ""
+
+mckey = "response"
+mc = memcache.Client(['127.0.0.1:11211'], debug=0)
+
+def fetchUsers(page_size):
+    url = request_protocol + se_api_url + "users?pagesize=" + str(page_size) + "&order=desc&sort=reputation&site=" + target_site + "&key=" + api_key;
+    resp = requests.get(url=url, params={})
+    return json.loads(resp.text.encode('utf-8', 'replace'))
+
+def fetchAnswers(id):
+    url = request_protocol + se_api_url + "users/" + str(id) + "/answers?pagesize=5&order=desc&sort=votes&site=" + target_site + "&key=" + api_key;
+    resp = requests.get(url=url, params={})
+    return json.loads(resp.text.encode('utf-8', 'replace'))
+
+def fetchQuestion(id):
+    url = request_protocol + se_api_url + "questions/" + str(id) + "?order=desc&sort=activity&site=" + target_site + "&key=" + api_key;
+    resp = requests.get(url=url, params={})
+    return json.loads(resp.text.encode('utf-8', 'replace'))        
+
+def fetchTags(id):
+    url = request_protocol + se_api_url + "users/" + str(id) + "/top-tags?pagesize=5&site=" + target_site + "&key=" + api_key;
+    resp = requests.get(url=url, params={})
+    return json.loads(resp.text.encode('utf-8', 'replace'))
+    
+# '{"id":"", "username":"", "reputation":"", "avatar":"", "answers":[{"id":"", "title": "", "score":""},{"id":"","title":"","score":""}],"tags":["java","c++","c","c#","qt"]}';
+def fetchData(page_size):
+    cache = mc.get(mckey+str(page_size))
+    if cache is not None:
+        print "Using cache as the response"
+        return cache;
+    response = list()
+    users = fetchUsers(page_size).get('items', dict())
+    for index in range(0, len(users)):
+        user = users[index]
+        id = user['user_id']
+        username = user['display_name']
+        reputation = user['reputation']
+        avatar = user['profile_image']
+        
+        answer_list = list();
+        answer_resp = fetchAnswers(id)['items']
+        for answer_index in range(0, len(answer_resp)):
+            answer = answer_resp[answer_index]
+            answer_id = answer['answer_id']
+            score = answer['score']
+            question_id = answer['question_id']
+
+            question = fetchQuestion(question_id)['items'][0]
+            title = question['title']
+
+            answer_list.append({
+                "id": answer_id,
+                "score": score,
+                "title": title
+            }) 
+
+        tag_list = list()
+        tag_resp = fetchTags(id)['items']
+        for tag_index in range(0, len(tag_resp)):
+            tag = tag_resp[tag_index]
+            tag_name = tag['tag_name']
+
+            tag_list.append(tag_name)
+
+        response.append({
+            "id": id,
+            "username": username,
+            "reputation": reputation,
+            "avatar": avatar,
+            "answers": answer_list,
+            "tags": tag_list
+        })
+    result = json.dumps(response)
+    print mc.set(mckey+str(page_size), result, time=3600)    
+    return result
+                
+class CustomHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/json/users' or self.path == '/json/users/init':
+            self.send_response(200)
+            self.send_header('Content-type','application/json')
+            self.end_headers()
+            if self.path=='/json/users/init':
+                self.wfile.write(fetchData(1)) 
+            else:
+                 self.wfile.write(fetchData(PAGE_SIZE))
+            return
+        else:
+            SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)            
+
+if __name__ == "__main__":
+    os.chdir("./static/")
+    httpd = SocketServer.TCPServer(("", PORT), CustomHandler)
+    print "serving at port", PORT
+    httpd.serve_forever()
+    quit()
+
+sys.path.append(DOCUMENT_ROOT)
+
+def get_index():
+    f = open(DOCUMENT_ROOT +'/static/index.html', 'r')
+    return f.read()
+
+def application(environ, start_response):
+    output = ""
+    status = "200 OK"
+    content_type = "text/plain"
+    path = environ.get('PATH_INFO', None)
+
+    if path == '/' or path == "index.html":
+        output = get_index()
+        content_type = 'text/html'
+    elif path == '/json/users':
+        content_type = 'application/json'
+        output = fetchData(PAGE_SIZE)
+    elif path == '/json/users/init':
+        content_type = 'application/json'
+        output = fetchData(1)
+    
+    if output == "":    
+        status = "404 Not Found"
+        output = "Not Found"
+
+    response_headers = [('Content-type', content_type),
+                        ('Content-Length', str(len(output)))]
+    start_response(status, response_headers)
+
+    return [output]
---
+++ static/m/scripts.js
@@ -0,0 +1,187 @@
+
+var server_url = "localhost";
+var stackoverflow_url = "http://ru.stackoverflow.com";
+var init_service_url = "/json/users/init";
+var service_url = "/json/users";
+
+var post_type_answer = "answer";
+var post_type_question = "question";
+var post_type_comment = "comment";
+
+var answer_strings = ["ответ", "ответа", "ответов"];
+var score_strings = ["голос", "голоса", "голосов"];
+var view_strings = ["показ", "показа", "показов"];
+
+var posts_symbols;
+var posts_commands;
+
+var usersList = {
+    users: [],
+    current: 0
+};
+
+$(function() {
+    init();
+    var users_root = $("#users_root");
+});
+
+function init() {
+    posts_symbols = new Array(post_type_answer, post_type_question, post_type_comment);
+	posts_symbols[post_type_answer] = ["О", "отвечен"];
+    posts_symbols[post_type_question] = ["В", "задан"];
+    posts_symbols[post_type_comment] = ["К", "прокомментирован"];
+
+    requestUsers();
+    initEditor();
+    initArrows(); 
+
+    $("#ask_question").attr("href", stackoverflow_url + "/questions/ask/");
+}
+
+function initEditor() {
+    var buttons = $(".button_row .button");
+    var step = 20;
+    var space_added = 0;
+    for (var i = 0; i < buttons.length; i++) {
+        var button = buttons[i];
+        var add_space = 0;
+        if (i == 0){
+            add_space = 0;
+        } else if (i == 2 || i == 8 || i == 12) {
+            add_space = 2 * (step + 5);   
+        } else {
+            add_space = step + 5;
+        }
+        space_added += add_space;
+        $(button).attr("style", "left: " + space_added + "px;");
+
+        function doIt(span, img_pos_x) {
+            $(span).attr("style", "background-position: " + img_pos_x + "px 0px;");
+
+            $(span).hover(function(){
+                $(span).attr("style", "background-position: " + img_pos_x + "px -40px;");
+            }, function(){
+                $(span).attr("style", "background-position: " + img_pos_x + "px 0px;");
+            }); 
+        };
+        doIt($(button).find("span"), (i * step * -1));
+    }
+
+    $(buttons[buttons.length-1]).attr("style", "right: 0px;");
+}
+
+function requestUsers() {
+    $("#user_section").css("display", "none");
+    $.get(init_service_url, function(data){
+        createUsersFeed(data);
+        $("#user_section").css("display", "block");
+        $.get(service_url, function(data){
+            createUsersFeed(data);
+        });
+    });
+}
+
+function initArrows() {
+    $("#left_arrow").click(leftArroClick);
+    $("#right_arrow").click(rightArroClick);
+    updateArrows();
+}
+
+function updateArrows() {
+    if (usersList.users.length > 1){
+        $("#left_arrow").css("display", "block");
+        $("#right_arrow").css("display", "block");
+    } else {
+        $("#left_arrow").css("display", "none");
+        $("#right_arrow").css("display", "none");
+    }
+}
+
+function leftArroClick() {
+    var currentIndex = usersList.current;
+    var newIndex = -1;
+    if (currentIndex == 0) {
+        newIndex = usersList.users.length - 1;
+    } else {
+        newIndex = currentIndex - 1;
+    }
+    displayUser(newIndex);
+    usersList.current = newIndex;
+}
+
+function rightArroClick() {
+    var currentIndex = usersList.current;
+    var newIndex = -1;
+    if (currentIndex >= usersList.users.length-1) {
+        newIndex = 0;
+    } else {
+        newIndex = currentIndex + 1;
+    }
+    displayUser(newIndex);
+    usersList.current = newIndex;
+}
+
+function createUsersFeed(users) {
+    for (index = 0; index < users.length; index++) {
+        var is_existed = false;
+        user = users[index];
+        for (sub_index = 0; sub_index < usersList.users.length; sub_index++) { 
+            if (usersList.users[sub_index].id == user.id) {
+                is_existed = true;
+                break;
+            }
+        }
+        if (!is_existed) {
+            usersList.users.push(user);
+        }
+    }
+
+    if (usersList.users.length > 0)
+        displayUser(0);
+    updateArrows();
+}
+
+function displayUser(index) {
+    var user = usersList.users[index];
+    
+    $("#user_profile").attr("href", stackoverflow_url + "/users/" + user.id);
+    $("#avatar").attr("src", user.avatar);
+    $("#username").text(user.username);
+    $("#reputation").text(user.reputation);
+    
+    var answers = user.answers;
+    var answer_html = ""; 
+    for (var i = 0; i < answers.length; i++) {
+        var answer = answers[i];
+        var tmp = answerTemplate();
+        var template = $(tmp);
+        var link = stackoverflow_url + "/a/" + answer.id + "/";
+        template.find(".answer").attr("onclick", "window.location.href='" + link + "'");
+        template.find(".score").text(answer.score);
+        template.find(".title").text(answer.title);
+        template.find(".title").attr("href", link);
+        answer_html += template.html(); 
+    }
+    $("#answer_root").empty();
+    $("#answer_root").html(answer_html);
+
+    var tags = user.tags;
+    var tag_html = "";
+    for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i];
+        var tmp = tagTemplate();
+        var template = $(tmp);
+        template.find(".post_tag").text(tag);
+        tag_html += template.html();
+    } 
+    $("#tag_root").empty();
+    $("#tag_root").html(tag_html);
+}
+
+function answerTemplate() {
+    return '<div><div class="answer"><span class="score"></span><a class="title"></a></div></div>'; 
+}
+
+function tagTemplate() {
+    return '<div><span class="post_tag"></span></div>';
+}
